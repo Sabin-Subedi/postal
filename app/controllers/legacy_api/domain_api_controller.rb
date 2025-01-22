e183931b102d2619d4e9f094e1f1afa5a8875c22
Sabin Subedi <sabinsubedi125@gmail.com>
2025-01-22 21:43:12 +0545

    feat: update specs

---
+++ app/controllers/legacy_api/domain_api_controller.rb
@@ -0,0 +1,132 @@
+# frozen_string_literal: true
+
+module LegacyAPI
+  class DomainsController < BaseController
+    # Create a domain
+    #
+    #   URL:            /api/v1/domains/create
+    #
+    #   Parameters:     name            => REQ: The name of the domain (max 50 characters)
+    #
+    #   Response:       A hash containing the domain information
+    #                   OR an error if the domain could not be created.
+    #
+    def create
+      if api_params["name"].blank?
+        render_parameter_error "`name` parameter is required but is missing"
+        return
+      end
+
+      domain = @current_credential.server.domains.find_by_name(api_params["name"])
+      if domain.nil?
+        domain = Domain.new(
+          server: @current_credential.server,
+          name: api_params["name"],
+          verification_method: "DNS",
+          owner_type: Server,
+          owner_id: @current_credential.server.id,
+          verified_at: Time.now
+        )
+
+        if domain.save
+          render_success domain: domain_to_hash(domain)
+        else
+          error_message = domain.errors.full_messages.first
+          if error_message == "Name is invalid"
+            render_error "InvalidDomainName", message: "The provided domain name is invalid"
+          else
+            render_error "UnknownError", message: error_message
+          end
+        end
+      else
+        render_error "DomainNameExists", message: "The domain name already exists"
+      end
+    end
+
+    # Query a domain
+    #
+    #   URL:            /api/v1/domains/query
+    #
+    #   Parameters:     name            => REQ: The name of the domain to query
+    #
+    #   Response:       A hash containing the domain information
+    #                   OR an error if the domain does not exist.
+    #
+    def query
+      if api_params["name"].blank?
+        render_parameter_error "`name` parameter is required but is missing"
+        return
+      end
+
+      domain = @current_credential.server.domains.find_by_name(api_params["name"])
+      if domain.nil?
+        render_error "DomainNotFound", message: "No domain found matching the provided name"
+      else
+        render_success domain: domain_to_hash(domain)
+      end
+    end
+
+    # Check domain status
+    #
+    #   URL:            /api/v1/domains/check
+    #
+    #   Parameters:     name            => REQ: The name of the domain to check
+    #
+    #   Response:       A hash containing the domain status information
+    #                   OR an error if the domain does not exist.
+    #
+    def check
+      if api_params["name"].blank?
+        render_parameter_error "`name` parameter is required but is missing"
+        return
+      end
+
+      domain = @current_credential.server.domains.find_by_name(api_params["name"])
+      if domain.nil?
+        render_error "DomainNotFound", message: "No domain found matching the provided name"
+      else
+        domain.check_dns(:manual)
+        render_success domain: domain_to_hash(domain)
+      end
+    end
+
+    # Delete a domain
+    #
+    #   URL:            /api/v1/domains/delete
+    #
+    #   Parameters:     name            => REQ: The name of the domain to delete
+    #
+    #   Response:       A success message if deleted successfully
+    #                   OR an error if the domain could not be deleted.
+    #
+    def delete
+      if api_params["name"].blank?
+        render_parameter_error "`name` parameter is required but is missing"
+        return
+      end
+
+      domain = @current_credential.server.domains.find_by_name(api_params["name"])
+      if domain.nil?
+        render_error "DomainNotFound", message: "No domain found matching the provided name"
+      elsif domain.delete
+        render_success message: "Domain deleted successfully"
+      else
+        render_error "DomainNotDeleted", message: "Domain could not be deleted"
+      end
+    end
+
+    private
+
+    # Helper method to convert a domain object into a hash
+    def domain_to_hash(domain)
+      {
+        id: domain.id,
+        name: domain.name,
+        verification_method: domain.verification_method,
+        owner_type: domain.owner_type,
+        owner_id: domain.owner_id,
+        verified_at: domain.verified_at&.to_f
+      }
+    end
+  end
+end
